a3a9bf1d7731871c5167632a2fadeefa153cfc3f
Brent-Honeycutt <131912805+Brent-Honeycutt@users.noreply.github.com>
2023-05-03 16:31:08 -0400

    Update calculator.cy.js

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -24,4 +24,18 @@ describe('Calculator', () => {
     // Verify that the submitted data is displayed in the console log
     cy.get('[data-cy="calculator-result"]').should('contain', 'Result: 9');
   });
+  it('performs subtraction correctly', () => {
+    
+    // Enter "2" in the first calculator field
+    cy.get('#num1').type('10');
+
+    // Enter "7" in the second calculator field
+    cy.get('#num2').type('7');
+
+    // Select the "+" button to add the numbers
+    cy.get('button').contains('-').click();
+
+    // Verify that the submitted data is displayed in the console log
+    cy.get('[data-cy="calculator-result"]').should('contain', 'Result: 3');
+  });
 });
