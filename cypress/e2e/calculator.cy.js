b63908f74fa7155a80ebe6cbed7d65af5cc27818
Brent-Honeycutt <131912805+Brent-Honeycutt@users.noreply.github.com>
2023-05-04 12:40:38 -0400

    Added more tests

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -10,6 +10,28 @@ describe('Calculator', () => {
       }
     });
   })
+  it('can input numbers in the fields', () => {
+
+    //Enter "1234567890" in both fields
+    cy.get('#num1').type('1234567890');
+    cy.get('#num2').type('1234567890');
+
+    //Verify that "1234567890" in the value of both fields
+    cy.get('#num1').should('have.value', '1234567890')
+    cy.get('#num1').should("have.value", '1234567890')
+  });
+
+  it('can NOT input letters, symbols, or emoji in the fields', () => {
+
+    //Enter "abyz&^~)}?/😎" in both fields
+    cy.get('#num1').type('abyz&^~)}?/😎');
+    cy.get('#num2').type('abyz&^~)}?/😎');
+
+    //Verify both fields remain blank, disallowing the characters
+    cy.get('#num1').should('have.value', '')
+    cy.get('#num1').should('have.value', '')
+  })
+ 
   it('performs addition correctly', () => {
     
     // Enter "2" in the first calculator field
@@ -24,6 +46,7 @@ describe('Calculator', () => {
     // Verify that the submitted data is displayed in the console log
     cy.get('[data-cy="calculator-result"]').should('contain', 'Result: 9');
   });
+
   it('performs subtraction correctly', () => {
     
     // Enter "2" in the first calculator field
@@ -38,6 +61,7 @@ describe('Calculator', () => {
     // Verify that the submitted data is displayed in the console log
     cy.get('[data-cy="calculator-result"]').should('contain', 'Result: 3');
   });
+
   it('performs multiplication correctly', () => {
     
     // Enter "2" in the first calculator field
@@ -51,5 +75,20 @@ describe('Calculator', () => {
 
     // Verify that the submitted data is displayed in the console log
     cy.get('[data-cy="calculator-result"]').should('contain', 'Result: 70');
+  });
+
+  it('performs division correctly', () => {
+    
+    // Enter "2" in the first calculator field
+    cy.get('#num1').type('9258');
+
+    // Enter "7" in the second calculator field
+    cy.get('#num2').type('3');
+
+    // Select the "+" button to add the numbers
+    cy.get('button').contains('/').click();
+
+    // Verify that the submitted data is displayed in the console log
+    cy.get('[data-cy="calculator-result"]').should('contain', 'Result: 3086');
   });
 });
